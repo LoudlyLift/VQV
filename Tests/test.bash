318770f304ddcf421fab337159feea41504f8ac8
Ivan Johnson <git@IvanJohnson.net>
2019-05-07 13:25:12 -0500

    Rewrite tests

--- Tests/test.bash
+++ Tests/test.bash
@@ -1,46 +1,13 @@
 #!/bin/bash
-MAX_COUNT="$1"
 
-IN_SUFFIX=".yuv"
-OUT_SUFFIX=".hevc"
-TEST_DIR="$HOME/UA/CurrSem/VQV/Kvazaar/Tests/"
-RAW_DIR="$TEST_DIR/Raw"
-REF_DIR="$TEST_DIR/Standard"
-TMP_DIR="/tmp/test_kvazaar"
-/bin/rm -rf "$TMP_DIR"
-mkdir -p "$TMP_DIR"
+WDIR=$(mktemp -d)
 
-if [ "$MAX_COUNT" = "" ]; then
-	MAX_COUNT=-1;
-fi
-
-echo Hello, World!
-
-#Build Kvazaar
-pushd "$HOME/UA/CurrSem/VQV/Kvazaar/"
-echo "###BUILDING###"
-make -j
-sudo make install
-
-#Encode test files
-echo "###TESTING###"
-cd "$RAW_DIR"
-for x in *.yuv; do
-	if [ $MAX_COUNT -eq 0 ]; then
-		break
-	fi
-	fIn="./$x"
-	fOut="${TMP_DIR}/${x/$IN_SUFFIX}$OUT_SUFFIX"
-	fRef="${REF_DIR}/${x/$IN_SUFFIX}$OUT_SUFFIX"
-
-	kvazaar -i "$fIn" -o "$fOut" > /dev/null 2>&1 && echo "$x completed" || (echo failed "$x"; break);
-
-	fInSize=$(du "$fIn" | cut -f 1)
-	fOutSize=$(du "$fOut" | cut -f 1)
-	fRefSize=$(du "$fRef" | cut -f 1)
-
-	prcnt=$((100*$fOutSize/$fRefSize))
-	echo "$x is ${prcnt}% the size of the reference implementation"
-
-	MAX_COUNT=$((MAX_COUNT - 1))
+for file in $(find ./ -type f \( -iname \*.hevc -o -iname \*.mp4 \)); do
+	FNAME=$(rev <<< "$file" | cut -d '/' -f 1 | rev)
+	OF="$WDIR/$FNAME"
+	../bin/script.bash "$file" "$OF"
+	du -s "$file" >> inputs.txt
+	du -s "$OF" >> outputs.txt
 done
+
+rm -r "$WDIR"
